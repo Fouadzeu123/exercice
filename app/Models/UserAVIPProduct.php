8e60eebc69db3af806d0062602bf6a4b04e7802a
Fouadzeu <borisange81@gmail.com>
2026-06-04 12:37:38 +0100

    petite modification

--- app/Models/UserAVIPProduct.php
+++ app/Models/UserAVIPProduct.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserAVIPProduct extends Model
 {
+    protected $table = 'user_avip_products';
     protected $guarded = [];
 
     protected $casts = [
